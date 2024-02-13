ac166145f5143e59d536582361a8a687d8357171
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-02-13 15:53:10 +0100

    Optimisation CSS + create JS

---
+++ js/components.js
@@ -0,0 +1,6 @@
+// Sons
+const wall_hit = new Audio('../sounds/bloop.mp3');
+const paddle_hit = new Audio('../sounds/bong.mp3');
+const brick_hit = new Audio('../sounds/boum.mp3');
+const win = new Audio('../sounds/applauses.mp3');
+const die = new Audio('../sounds/casse.mp3');
