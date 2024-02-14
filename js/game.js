43d441c2ab41c066d824dbeca1115ff05bf8fef0
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-02-14 14:41:07 +0100

    Affichage règles du jeu et créer paddle

--- js/game.js
+++ js/game.js
@@ -1,2 +1,42 @@
-//Jeu général.
-console.log("Le script général charge");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const rules = document.getElementById("rules");
+const rulesButton = document.getElementById("rules-btn");
+const closeRules = document.getElementById("close-btn");
+//variables
+let leftArrow = false;
+let rightArrow = false;
+
+//gestion affichage fonctionnement
+rulesButton.addEventListener("click", () =>{
+    rules.classList.add("show");
+});
+closeRules.addEventListener("click", () =>{
+    rules.classList.remove("show");
+});
+
+//Contole de la planche avec touches fléchés du clavier
+document.addEventListener("keydown", (k) => {
+    if (k.key == "ArrowLeft" || k.key == "Left"){
+        leftArrow = true;
+    } else if (k.key == "ArrowRight" || k.key == "Right"){
+        rightArrow = true;
+    }
+});
+document.addEventListener("keyup", (k) => {
+    if (k.key == "ArrowLeft" || k.key == "Left"){
+        leftArrow = false;
+    } else if (k.key == "ArrowRight" || k.key == "Right"){
+        rightArrow = false;
+    }
+});
+
+function loop(){
+    drawPaddle();
+    movePaddle();
+    requestAnimationFrame(loop());
+}
+
+window.onload = (event) => {
+    loop();
+};
--- js/paddle.js
+++ js/paddle.js
@@ -1,2 +1,46 @@
-//créer la planche ici
-console.log("Le paddle charge");
+//variables
+//largeur de la planche
+let paddle_width = 100;
+//placement de la planche
+const paddle_margin_bottom = 20;
+//hauteur de la planche
+const paddle_height = 10;
+//vitesse déplacement de la planche
+let speedPaddle = 8;
+
+//créer la planche
+let paddle = {
+    //centre la planche, moitié largeur canvas - moitié largeur planche.
+    x: (canvas.width /2) - (paddle_width /2),
+    //place la planche en hauteur
+    y: canvas.height - paddle_margin_bottom - paddle_height,
+    //prends la largeur actuelle de la planche
+    w: paddle_width,
+    //prends la hauteur de la planche
+    h: paddle_height,
+    //vitesse de déplacement de la planche
+    dx: speedPaddle
+}
+
+//dessiner la planche
+function drawPaddle(){
+    //prends le modèle 2D de la planche
+    ctx.beginPath();
+    //change la couleur de planche (ou texture)
+    ctx.fillStyle = "#da87f1";
+    //dessinner la planche
+    ctx.fillRect(paddle.x, paddle.y, paddle.w, paddle.h);
+    //couleur de bordure
+    ctx.strokeStyle = "#e9a0ff";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.closePath();
+}
+
+//déplacer la planche
+function movePaddle(){
+    if (leftArrow) {
+        paddle.x -= paddle.dx
+    } else if (rightArrow){
+        paddle.x += paddle.dx
+    }
+}
