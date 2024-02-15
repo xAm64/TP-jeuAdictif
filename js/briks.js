d094f20f7250a8d0d4cdec59b3c7aa402763cdd0
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-15 13:27:27 +0100

    Add component Brick.js

--- js/briks.js
+++ js/briks.js
@@ -1,2 +1,36 @@
-//créer briques ici
-console.log("Les briques chargent");
+const brickWidth = 60;
+const brickHeight = 20;
+const brickMargin = 10;
+let positionY = 30;
+let row = 3;
+let bricks = [];
+let container = document.getElementById('gameSpace');
+
+function createBrick(){
+    for(let i = 0; i < row; i++){
+        let positionX = 20 + (i * 10);
+        let brickCount = Math.random() * (7 - 2) + 2;
+        positionY += i;
+        for(let j = 0; j < brickCount; j++){
+            let newBrick = document.createElement('div');
+            newBrick.setAttribute('class', "brick");
+            newBrick.style.width = brickWidth + 'px';
+            newBrick.style.height = brickHeight + 'px';
+            newBrick.style.left = positionX + 'px';
+            newBrick.style.top = positionY + 'px';
+            container.appendChild(newBrick);
+            
+            positionX += brickWidth + brickMargin;
+
+            bricks.push(newBrick);
+        }
+        positionY += brickHeight + brickMargin;
+    }
+    
+}
+
+createBrick();
+
+
+
+    
